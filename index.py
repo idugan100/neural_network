b00800073f509ea7ee71660615e6ec1fceadc649
isaac <isaacd4444@gmail.com>
2022-10-01 21:51:23 -0400

    batch output generation

--- index.py
+++ index.py
@@ -3,16 +3,20 @@ import numpy as np
 import matplotlib
 
 
-inputs = [1 ,2 ,3,2.5]
-weights1 = [0.2, 0.8, -0.5,1]
-weights2 = [0.5, -.91, .26,-.5]
-weights3 = [-.26, -0.27,.17,.87]
-bias1=2
-bias2=3
-bias3=0.5
+inputs = [[1.0 ,2.0 ,3.0 ,2.5],
+            [2.0,5.0,-1.0,2.0],
+            [-1.5,2.7,3.3,-0.8]] 
+            #batch inputs to allow for generalizations 
+            #of error, but dont add the enitre sample set otherwise it will fit but 
+            #not generalize
+weights=[[0.2, 0.8, -0.5,1],[0.5, -.91, .26,-.5],[-.26, -0.27,.17,.87]]
+biases=[2,3,.5]
 
-output1=np.dot(inputs,weights1)+bias1
-output2=np.dot(inputs,weights2)+bias2
-output3=np.dot(inputs,weights3)+bias3
-outputs=[output1,output2,output3]
-print(outputs)
+
+## transpose weights so matrix multiplication works each row of the result is
+#the set of outputs from the corresponding input
+output=np.dot(inputs,np.array(weights).T)+biases
+print(output)
+
+    
+
